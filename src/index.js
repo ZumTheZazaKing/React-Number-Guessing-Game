8cb33e946f6bf35bf1ffe739d2d0dbe6205f8f68
ZumTheZazaKing <zahidi85543@gmail.com>
2021-08-01 17:33:55 +0800

    Initial layout of app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,69 @@
 import React from 'react';
+import { useState,useRef } from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import reportWebVitals from './reportWebVitals';
 
 
+function MainMenu(){
+  return <div id="mainMenu">
+    <h2>Number Guessing Game</h2>
+    <form>
+      <input type="text" placeholder="Enter Name" required/>
+      <br/><br/>
+      <input type="submit" value="Start"/>
+    </form>
+  </div>;
+}
+
+function DifficultyMenu(){
+  return <div id="difficulty">
+    <p>Greetings, !</p>
+    <h3>Difficulty</h3>
+    <button>Easy</button>
+    <button>Normal</button>
+    <button>Hard</button>
+  </div>;
+}
+
+function Ingame(){
+  return <div id="ingame">
+    <h3>Guess The Number!</h3>
+    <p>Current Range: </p>
+    <div id="bar">
+      <div id="limitBefore"></div>
+      <div id="range"></div>
+      <div id="limitAfter"></div>
+    </div>
+    <p id="chances">Chances: </p>
+    <form>
+      <input type="number" min="0" max="100" required/>
+      <input type="submit" value="Guess"/>
+    </form>
+  </div>;
+}
+
+function Result(){
+  return <div id="result">
+    <h3>Congrats! You guessed the number</h3>
+    <p>The number was </p>
+    <button>Play Again</button>
+  </div>;
+}
+
+function Leaderboard(){
+  return <div></div>;
+}
+
+
 function App(){
 
-  return <div>
+  return <div id="container">
 
-    <h1>Hello</h1>
+    <MainMenu/>
+    <DifficultyMenu/>
+    <Ingame/>
+    <Result/>
 
   </div>
 
